797a8b24ba9d1bcc64d81b617c132d515c423b8e
NguyenPham26 <phamthanhnguyen1080@gmail.com>
2022-09-29 10:58:41 +1000

    create a new exercise

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+
+const express = require('express');
+// const cors = require('cors')
+const mongoose = require('mongoose');
+const workoutRoutes = require('./routes/workouts');
+
+// express app
+const app = express();
+
+// middleware
+app.use(express.json());
+// app.use(cors())
+
+//routes
+app.use('/api/workouts', workoutRoutes);
+
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    //listen for request
+    app.listen(process.env.PORT, () => {
+      console.log('listening on port 4000');
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
